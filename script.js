8ac59282e2ee5ea9142ad544278a2a62cdc53e2a
wendy neo <jiayi0621@gmail.com>
2023-12-18 13:15:14 +0000

    add hawkers

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", async function(){
     attractionLayerGroup.addTo(map);
     for (let attraction of attractions) {
         const marker = L.marker(
-            [attraction.latitude, attraction.longitude]
+            [attraction.latitude, attraction.longitude], { icon: createMarkerIcon('red') }
         );
         marker.bindPopup(`<b>${attraction.name}</b><br>${attraction.description}`);
         marker.on('click', () => {
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", async function(){
     hawkerLayerGroup.addTo(map);
     for (let h of hawkers){
         const marker = L.marker(
-            [h.latitude, h.longitude]
+            [h.latitude, h.longitude], { icon: createMarkerIcon('blue') }
         )
         marker.bindPopup(`<b>${h.name}</b><br>${h.description}`);
         marker.on('click', () => {
@@ -133,6 +133,19 @@ document.addEventListener("DOMContentLoaded", async function(){
         updateSidePanel(location);
         openSidePanel();
         map.flyTo([location.latitude, location.longitude], 15);
+    }
+
+    function createMarkerIcon(color) {
+        return L.icon({
+            iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            tooltipAnchor: [16, -28],
+            shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+            shadowSize: [41, 41],
+            shadowAnchor: [12, 41],
+        });
     }
 });
 
